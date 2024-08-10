98ac72d1115bc7215ff9b87324dffd7e76b55bbb
Max-GO-IT <terletskymaxim@ukr.net>
2024-08-10 22:23:01 +0300

    ....

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,27 +6,31 @@ import Description from '../Description/Description';
 import Options from '../Options/Options';
 import Notification from '../Notification/Notification';
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [feedbackType,setFeedbackType]=useState({good:0,neutral:0,bad:0,})
   const [total, setTotal] = useState(0);
   const [positive, setPositive] = useState(0);
 
+  const onLeaveFeedback = (option) => {
+    setFeedbackType({
+      ...feedbackType,
+      [option]: feedbackType[option] + 1
+    });
+  };
+
   useEffect(() => {
-    const newTotal = good + neutral + bad;
-    const newPositive = newTotal ? (good / newTotal) * 100 : 0;
+    const newTotal = feedbackType.good + feedbackType.neutral + feedbackType.bad;
+    const newPositive = newTotal ? ((feedbackType.good / newTotal) * 100).toFixed(2) : 0;
     setTotal(newTotal);
     setPositive(newPositive);
-  }, [good, neutral, bad]);
+  }, [feedbackType]);
 
-  const onGood = () => setGood(good + 1);
-  const onNeutral = () => setNeutral(neutral + 1);
-  const onBad = () => setBad(bad + 1);
+  const onGood = () => onLeaveFeedback('good');
+  const onNeutral = () => onLeaveFeedback('neutral');
+  const onBad = () => onLeaveFeedback('bad');
+  
   const onReset = () => {
-    setGood(0);
-    setNeutral(0);
-    setBad(0);
-    
+    setFeedbackType({good:0,neutral:0,bad:0,})
+   
   };
     
 
@@ -34,7 +38,7 @@ function App() {
     <>
       <Description/>
       <Options onBad={onBad} onGood={onGood} onNeutral={onNeutral} onReset={onReset} total={total}/>
-      { total===0 ? <Notification/> :<Feedback  good={good} neutral={neutral} bad={bad} total={total} positive={positive} /> }
+      { total===0 ? <Notification/> :<Feedback  good={feedbackType.good} neutral={feedbackType.neutral} bad={feedbackType.bad} total={total} positive={positive} /> }
       
     </> 
     
